38722927f34e2d2952b04ae10491c0551c059e15
Cristian Gutu <hey@cristiangutu.pro>
2024-03-18 18:21:26 +0200

    chore: naming update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
-import { setAppBadgeCount } from 'react-native-app-badge';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { setAppBadgeCount } from '@guulabs/react-native-app-badge';
 
 export default function App() {
-
   React.useEffect(() => {
-    setAppBadgeCount(5)
+    setAppBadgeCount(5);
   }, []);
 
-  return null;
+  return (
+    <View
+      style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}
+    >
+      <TouchableOpacity onPress={() => setAppBadgeCount(0)}>
+        <Text>Clear app badge</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setAppBadgeCount(5)}>
+        <Text>Set app badge</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
